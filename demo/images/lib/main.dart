abc93270a934e55d736df508d2079b16500eb2a6
Robert Felker <felker.robert@scub.net>
2023-03-17 16:09:08 +0100

    maj du lien images

--- demo/images/lib/main.dart
+++ demo/images/lib/main.dart
@@ -7,14 +7,15 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(const ImageApp());
 
+/// Objectif: Explorer les différents modes d'affiches d'images
 class ImageApp extends StatelessWidget {
   const ImageApp({super.key});
 
   @override
   build(_) =>
       // Un conteneur externe d'une taille précise
-  Center(
-      child: Column(
+      Center(
+          child: Column(
         children: [
           Container(
               width: 350,
@@ -24,15 +25,14 @@ class ImageApp extends StatelessWidget {
                 // L'image s'adapte pour occuper la hauteur maximale
                 fit: BoxFit.fitHeight,
                 // Le chargement se fait depuis une ressource externe
-                child: Image.network(
-                    'https://main--marvelous-crisp-dc8bf2.netlify.app/design/assets/pi_0.png'),
+                child: Image.network('/images/scub-logo.webp'),
               )),
           Container(
             width: 300,
             height: 200,
             color: Colors.blue.shade100,
             child: FittedBox(
-              // L'image s'adapte pour occuper la hauteur maximale
+                // L'image s'adapte pour occuper la hauteur maximale
                 fit: BoxFit.fitWidth,
                 // Le chargement se fait depuis une ressource locale
                 child: Image.asset('/assets/turtle.jpeg')),
