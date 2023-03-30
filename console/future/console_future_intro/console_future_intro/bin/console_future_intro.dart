a3659b58be089197e233e21bcae6f2d2db4e8b6a
Robert Felker <felker.robert@scub.net>
2023-03-30 17:58:46 +0200

    rebasing

---
+++ console/future/console_future_intro/console_future_intro/bin/console_future_intro.dart
@@ -0,0 +1,5 @@
+import 'package:console_future_intro/console_future_intro.dart' as console_future_intro;
+
+void main(List<String> arguments) {
+  print('Hello world: ${console_future_intro.calculate()}!');
+}
