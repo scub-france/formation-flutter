6943c966be05f507e4bba408206c35096f261bc0
Robert Felker <felker.robert@scub.net>
2023-03-30 18:08:16 +0200

    création console_future_isolate

--- console/future/console_future_intro/console_future_intro/bin/console_future_intro.dart
+++
@@ -1,5 +0,0 @@
-import 'package:console_future_intro/console_future_intro.dart' as console_future_intro;
-
-void main(List<String> arguments) {
-  print('Hello world: ${console_future_intro.calculate()}!');
-}
