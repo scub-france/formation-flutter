026544014964606577299512ac2e845a1429b9a2
Robert Felker <felker.robert@scub.net>
2023-03-31 09:51:21 +0200

    shell by rollback

--- console/future/shell/bin/shell.dart
+++
@@ -1,5 +0,0 @@
-import 'package:shell/shell.dart' as shell;
-
-void main(List<String> arguments) {
-  print('Hello world: ${shell.calculate()}!');
-}
--- console/future/shell/lib/shell.dart
+++ console/future/shell/lib/shell.dart
@@ -1,3 +1,6 @@
-int calculate() {
-  return 6 * 7;
+Future<void> main() async {
+  for (var i = 0; i < 5; i++) {
+    print('Hello from Dart ($i)!');
+    await Future.delayed(Duration(milliseconds: 500));
+  }
 }
