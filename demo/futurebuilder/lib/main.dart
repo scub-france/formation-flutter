b590e51053814b7f76178b277c536f48551a1d09
Robert Felker <felker.robert@scub.net>
2023-03-31 11:32:42 +0200

    Review

--- demo/futurebuilder/lib/main.dart
+++ demo/futurebuilder/lib/main.dart
@@ -1,59 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(FutureBuild());
+  runApp(MaterialApp(
+      title: 'Future Builder',
+      theme: ThemeData(primarySwatch: Colors.grey),
+      home: Scaffold(body: Center(child: FutureBuild()))));
 }
 
+/// Démonstration du Widget d'exploitation des Futures.
 class FutureBuild extends StatelessWidget {
   FutureBuild({super.key});
 
-  /// Crée un FUTURE afin de simuler un chargement (une requête HTTP par exemple)
-  /// ici je renvoie un string 'Data load' après 5 secondes
-  /// Mais on peut aussi remplacer 'Data loading' par Future.error('Error') pour simuler une erreur
+  // Crée un FUTURE afin de simuler un chargement (une requête HTTP par exemple)
+  // Renvoie d'un string 'Donnée chargée' après 5 secondes
+  // On peut émettre Future.error('Error') pour simuler une erreur
   final Future<String> _data = Future<String>.delayed(
     const Duration(seconds: 5),
-    () => 'Data load',
+    () => 'Donnée chargée',
   );
 
   @override
-  build(context) => MaterialApp(
-        title: 'Future Builder',
-        theme: ThemeData(
-          primarySwatch: Colors.grey,
-        ),
-        home: Scaffold(
-            body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              // Crée mon Widget FutureBuilder en lui passant mon Future
-              // et je lui passe dans mon builder un AsyncSnapshot qui contient
-              // les données et l'etat de mon Future
-              FutureBuilder(
-                future: _data,
-                builder: (_, snapshot) {
-                  // Si mon Future a des données, je les affiche ou je peux faire un traitement
-                  if (snapshot.hasData) {
-                    return Text('${snapshot.data}');
-                  }
-                  // Si mon Future a une erreur, je l'affiche ou je peux faire un traitement
-                  else if (snapshot.hasError) {
-                    return Text('${snapshot.error}');
-                  }
-                  // Sinon je renvoie un Widget de chargement
-                  else {
-                    return Column(
-                      children: const [
-                        Text('Loading'),
-                        // Widget de chargement
-                        CircularProgressIndicator(),
-                      ],
-                    );
-                  }
-                },
-              ),
-            ],
+  build(_) => Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Crée mon Widget FutureBuilder en lui passant mon Future
+          // et je lui passe dans mon builder un AsyncSnapshot qui contient
+          // les données et l'etat de mon Future
+          FutureBuilder<String>(
+            future: _data,
+            builder: (_, snap) {
+              // Si mon Future a des données, je les affiche ou je peux faire un traitement
+              if (snap.hasData) {
+                return Text('Message: ${snap.data}');
+              }
+              // Si mon Future a une erreur, je l'affiche ou je peux faire un traitement
+              else if (snap.hasError) {
+                return Text('${snap.error}');
+              }
+              // Sinon je renvoie un Widget de chargement
+              else {
+                return Column(
+                  children: const [
+                    Text('Loading'),
+                    // Widget de chargement
+                    CircularProgressIndicator(),
+                  ],
+                );
+              }
+            },
           ),
-        )),
+        ],
       );
 }
