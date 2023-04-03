01bab6509c538a969b2adb50b7895b1a8925989a
Robert Felker <97283674+Robert-Scub@users.noreply.github.com>
2023-04-03 11:07:51 +0200

    Demo focus (#71)
    * exemple NodeFocus, il manque les commentaires
    
    * readme
    
    * readme
    
    * bonne pratique SCUB
    
    * readme
    
    * readme
    
    * readme
    
    * plus de commentaires
    
    * Refactor focus
    
    * demo focusScope, in progress....
    
    * clean repository
    
    * clean repository
    
    * refactor + commentaires
    
    * petite introduction a focusScope (readme)
    
    * clean code (ADD const modifier)
    
    * ajout du setState a _handleFocusChange() et restauration du readme
    
    * changer commentaire pour
    _nodeAttachment.reparent()
    
    * clean code (ADD const modifier)
    
    * First commit in progress(UI+ filtre chiffres/consonnes+ clean code....)
    
    * ignorer la touche Tab pour garder sa fonctionalitée
    
    * sauvegarde in progress
    
    * sauvegarde in progress
    
    * sauvegarde in progress
    
    * sauvegarde in progress
    
    * Quelques commentaires
    
    * Quelques commentaires
    
    * petite intro =>readme.md
    
    * petite intro =>readme.md
    
    * petite intro =>readme.md
    
    * RM Tests dir, correction de layout pour scope
    
    * coquille
    
    * Préparation de l'exo evt clavier
    
    * petite intro =>readme.md
    
    * rectifier les commentaires avec 3 /
    
    * rectifier les commentaires avec 3/
    
    ---------
    
    Co-authored-by: karim-lamtara <karim.lamtara@scub.net>
    Co-authored-by: Robert Felker <felker.robert@scub.net>
    Co-authored-by: karim-lamtara <122789979+karim-lamtara@users.noreply.github.com>

---
+++ demo/focus/focus_evenement_clavier/lib/main.dart
@@ -0,0 +1,252 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+/// Je vous propose cet exemple qui contient trois filtres.
+/// Chaque filtre a son noeud de focus et les trois noeuds ont comme
+/// parent notre widget principale qui contient le focus parent.
+/// On constatera ainsi comment chaque focus traite les évenements
+/// clavier et/ou les renvois au scope parent
+
+void main() => runApp(const MaterialApp(home: Scaffold(body: AlphaNumeriqueFilter())));
+
+/// Notre fabrique de filtres,
+class ChildButton extends StatefulWidget {
+  const ChildButton(
+      {super.key, required this.alphanumerique, required FocusNode focusParent, required this.affichageParDefaut});
+
+  final List<String> alphanumerique;
+  final String affichageParDefaut;
+
+  @override
+  State<ChildButton> createState() => _ChildButtonState();
+}
+
+class _ChildButtonState extends State<ChildButton> {
+  late FocusNode _node;
+  bool _focused = false;
+  late FocusAttachment _nodeAttachment;
+  Color _color = Colors.white;
+  String affichage = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _node = FocusNode();
+    _node.addListener(_handleFocusChange);
+    _nodeAttachment = _node.attach(context, onKey: _handleKeyPress);
+  }
+
+  void _handleFocusChange() {
+    if (_node.hasFocus != _focused) {
+      setState(() {
+        _color = _node.hasFocus ? Colors.lightBlueAccent : Colors.white;
+        affichage = "";
+        _focused = _node.hasFocus;
+      });
+    }
+  }
+
+  /// Cette methode est appelée a chaque evenement du clavier,
+  /// l'appel a la methode se fait grace a l'attribut onKey de
+  /// la methode attach voir plus haut dans initState()
+  KeyEventResult _handleKeyPress(FocusNode node, RawKeyEvent event) {
+    if (event is RawKeyDownEvent) {
+      /// on ignore l'evenement "Tab" pour garder la fonctionalité tabulation
+      if (event.logicalKey.keyLabel == "Tab") {
+        return KeyEventResult.ignored;
+      }
+      if (widget.alphanumerique.contains(event.logicalKey.keyLabel)) {
+        setState(() {
+          /// On traite le cas des numpad
+          affichage = affichage + event.logicalKey.keyLabel.replaceAll('Numpad ', '');
+          _color = Colors.green;
+        });
+
+        /// On prévient le FocusManager que l'évenement est traité et stoppera ainsi la propagation
+        return KeyEventResult.handled;
+      } else if (!widget.alphanumerique.contains(event.logicalKey.keyLabel)) {
+        setState(() {
+          _color = Colors.red;
+        });
+        return KeyEventResult.ignored;
+      }
+    }
+    setState(() {
+      _color = node.hasFocus ? Colors.lightBlueAccent : Colors.white;
+    });
+    return KeyEventResult.ignored;
+  }
+
+  /// oublions pas de liberer la memoire
+  @override
+  void dispose() {
+    _node.removeListener(_handleFocusChange);
+    // The attachment will automatically be detached in dispose().
+    _node.dispose();
+    super.dispose();
+  }
+
+  @override
+  build(_) {
+    _nodeAttachment.reparent();
+    return GestureDetector(
+      onTap: () {
+        if (_focused) {
+          _node.unfocus();
+        } else {
+          _node.requestFocus();
+        }
+      },
+      child: Center(
+        child: Container(
+          width: 400,
+          height: 50,
+          color: _color,
+          alignment: Alignment.center,
+          child: Text(_focused ? affichage : widget.affichageParDefaut),
+        ),
+      ),
+    );
+  }
+}
+
+/// Notre widget principal
+class ParentButton extends StatefulWidget {
+  const ParentButton({super.key});
+
+  @override
+  State<ParentButton> createState() => _ParentButton();
+}
+
+class _ParentButton extends State<ParentButton> {
+  late FocusNode _nodeParent;
+  late FocusAttachment _nodeAttachmentParent;
+  String titre = "Focus et les evenements clavier";
+  final affichageStatic = "Touche alphanumerique rejetée:";
+  String affichageDynamic = "";
+  final voyelles = ['A', 'E', 'Y', 'U', 'O', 'I'];
+  final consonnes = [
+    'Z',
+    'R',
+    'T',
+    'P',
+    'Q',
+    'S',
+    'D',
+    'F',
+    'G',
+    'H',
+    'J',
+    'K',
+    'L',
+    'M',
+    'W',
+    'X',
+    'C',
+    'V',
+    'B',
+    'N'
+  ];
+  final numerique = List<String>.generate(10, (int index) => index.toString()) +List<String>.generate(10, (int index) => "Numpad $index");
+
+  @override
+  void initState() {
+    super.initState();
+    _nodeParent = FocusNode();
+    _nodeAttachmentParent = _nodeParent.attach(context, onKey: _handleKeyPressParent);
+  }
+
+  /// Cette methode est appelée a chque evenement du clavier, l'appel a la methode se
+  /// fait grace a l'attribut onKey de la methode attach voir plus haut dans initState()
+  /// a ce niveau (noeud parent), seul les evenements ignorés pas le noeud enfant seront traités
+  KeyEventResult _handleKeyPressParent(FocusNode node, RawKeyEvent event) {
+    if (event is RawKeyDownEvent) {
+      /// on ignore l'evenement "Tab" pour garder la fonctionalité tabulation
+      if (event.logicalKey.keyLabel == "Tab") {
+        return KeyEventResult.ignored;
+      }
+
+      /// Ici, on traite les caractéres refusé par nos filtres
+      if (voyelles.contains(event.logicalKey.keyLabel) ||
+          consonnes.contains(event.logicalKey.keyLabel) ||
+          numerique.contains(event.logicalKey.keyLabel)) {
+        setState(() => affichageDynamic = event.logicalKey.keyLabel);
+      }
+      return KeyEventResult.handled;
+    }
+
+    /// Tout évenement qui ne parvient pas du clavier sera ignoré par notre widget et sera
+    /// ainsi traité a un niveau plus haut de l'application
+    return KeyEventResult.ignored;
+  }
+
+  /// N'oublions pas de dispose notre node
+  @override
+  void dispose() {
+    _nodeParent.dispose();
+    super.dispose();
+  }
+
+  @override
+  build(_) {
+    _nodeAttachmentParent.reparent();
+    return Center(
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+      Padding(
+        padding: const EdgeInsets.only(bottom: 150),
+        child: Text(titre),
+      ),
+
+      /// c'est ici qu'on crée le liens de parenté entre notre noeud parent et enfant
+      Focus(
+          parentNode: _nodeParent,
+          child: Padding(
+            padding: const EdgeInsets.all(25),
+            child: ChildButton(
+              alphanumerique: voyelles,
+              focusParent: _nodeParent,
+              affichageParDefaut: 'Filtre voyelles',
+            ),
+          )),
+
+      /// c'est ici qu'on crée le liens de parenté entre notre noeud parent et enfant
+      Focus(
+          parentNode: _nodeParent,
+          child: Padding(
+            padding: const EdgeInsets.all(25),
+            child: ChildButton(
+              alphanumerique: consonnes,
+              focusParent: _nodeParent,
+              affichageParDefaut: 'Filtre consonnes',
+            ),
+          )),
+
+      /// c'est ici qu'on crée le liens de parenté entre notre noeud parent et enfant
+      Focus(
+          parentNode: _nodeParent,
+          child: Padding(
+            padding: const EdgeInsets.all(25),
+            child: ChildButton(
+              alphanumerique: numerique,
+              focusParent: _nodeParent,
+              affichageParDefaut: 'Filtre chiffres',
+            ),
+          )),
+      Text(affichageStatic),
+      Text(
+        affichageDynamic,
+        style: const TextStyle(color: Colors.red, fontSize: 100),
+      )
+    ]));
+  }
+}
+
+class AlphaNumeriqueFilter extends StatelessWidget {
+  const AlphaNumeriqueFilter({super.key});
+
+  @override
+  build(context) => DefaultTextStyle(
+        style: Theme.of(context).textTheme.headlineMedium!,
+        child: const ParentButton(),
+      );
+}
--- demo/focus/focus_scope/lib/main.dart
+++ demo/focus/focus_scope/lib/main.dart
@@ -10,7 +10,7 @@ class FocusApp extends StatefulWidget {
 }
 
 class _FocusAppState extends State<FocusApp> {
-  final intruduction =
+  final introduction =
       "Voyons comment réagira notre application avec deux focusScope (groupes de focus).\n pour chaque focusScope, on lie un checkbox pour controller le parametre canRequestFocus de notre focusScope, on peut ainsi permettre/empécher le focusScope de demander le focus.";
   bool isScope1canRequestFocus = true;
   bool isScope2canRequestFocus = false;
@@ -18,7 +18,7 @@ class _FocusAppState extends State<FocusApp> {
   @override
   build(_) {
     final checkbox1 = Row(children: [
-      const Text('  Le scope ne peut demander le focus que si la case est cochée'),
+      const Text('Le scope ne peut demander le focus que si la case est cochée'),
       Checkbox(
           checkColor: Colors.white,
           value: isScope1canRequestFocus,
@@ -40,12 +40,20 @@ class _FocusAppState extends State<FocusApp> {
             });
           })
     ]);
-    const sizeBox= SizedBox(width:2, height:50);
-    // tout se joue ici ! on crée ainsi deux FocusScope...
+    const sizeBox = SizedBox(width: 2, height: 50);
+    /// tout se joue ici ! on crée ainsi deux FocusScope...
     final scope1 = FocusScope(canRequestFocus: isScope1canRequestFocus, child: const GroupeButtonWidget());
     final scope2 = FocusScope(canRequestFocus: isScope2canRequestFocus, child: const GroupeButtonWidget());
     return Center(
-      child: Column(children: [Text(intruduction),sizeBox,checkbox1, Expanded(child: scope1),sizeBox, checkbox2, Expanded(child: scope2)]),
+      child: Column(children: [
+        Text(intruduction),
+        sizeBox,
+        checkbox1,
+        Expanded(child: scope1),
+        sizeBox,
+        checkbox2,
+        Expanded(child: scope2)
+      ]),
 
     );
   }
@@ -61,13 +69,15 @@ class GroupeButtonWidget extends StatefulWidget {
 class _GroupeButtonWidget extends State<GroupeButtonWidget> {
   final children = List.generate(
       3,
-      (index) => const Padding(
-            padding: EdgeInsets.all(2.0),
-            child: FocusButton(),
-          ));
+          (index) =>
+      const Padding(
+        padding: EdgeInsets.all(2.0),
+        child: FocusButton(),
+      ));
 
   @override
-  build(_) => Scaffold(
+  build(_) =>
+      Scaffold(
         body: Center(
           child: Row(
             children: children,
@@ -91,50 +101,45 @@ class _FocusButton extends State<FocusButton> {
   bool get isFocused => _node.hasFocus;
   late FocusAttachment _nodeAttachment;
 
-  //Définissez le nœud de focus. Pour gérer le cycle de vie, créez le FocusNode dans
-  //la méthode initState et nettoyez-le dans la méthode dispose.
+  /// Définissez le nœud de focus. Pour gérer le cycle de vie, créez le FocusNode dans
+  /// la méthode initState et nettoyez-le dans la méthode dispose.
   @override
   void initState() {
     super.initState();
-    //Si vous souhaitez être averti chaque fois que Focus change, enregistrez un écouteur avec addListener
+    /// Si vous souhaitez être averti chaque fois que Focus change, enregistrez un écouteur avec addListener
     _node.addListener(_handleFocusChange);
     _nodeAttachment = _node.attach(context);
   }
 
   void _handleFocusChange() => setState(() => print("Focus updated to $isFocused"));
 
-  //nettoyez le nœud de focus lorsque le formulaire est éliminé
+  /// nettoyez le nœud de focus lorsque le formulaire est éliminé
   @override
   void dispose() {
-    //Vous devez vous désinscrire avec removeListener pour éviter les fuites de mémoire
+    /// Vous devez vous désinscrire avec removeListener pour éviter les fuites de mémoire
     _node.removeListener(_handleFocusChange);
-    // attachment sera detaché sur appel de dispose().
+    /// attachment sera detaché sur appel de dispose().
     _node.dispose();
     super.dispose();
   }
 
   @override
   build(_) {
-    //Garantit que le FocusNode attaché à ce point d'attachement a le bon nœud parent, en le modifiant si nécessaire.
+    /// Garantit que le FocusNode attaché à ce point d'attachement a le bon nœud parent, en le modifiant si nécessaire.
     _nodeAttachment.reparent();
 
-    return GestureDetector(
-      //permet d'avoir la main sur le focus grace au clics souris
+    return AspectRatio(aspectRatio: 1, child: GestureDetector(
+      /// permet d'avoir la main sur le focus grace au clics souris
       onTap: () => isFocused ? _node.unfocus() : _node.requestFocus(),
       child: Center(
         child: DecoratedBox(
           decoration: BoxDecoration(color: isFocused ? Colors.green : Colors.orangeAccent),
-          child: SizedBox(
-            width: 200,
-            height: 200,
-
-            child: Center(
-              child: isFocused ? const Text("Focus") : const SizedBox(),
-            ),
+          child: Center(
+            child: isFocused ? const Text("Focus") : Text("Click Me"),
           ),
         ),
       ),
-    );
+    ));
   }
 }
 
