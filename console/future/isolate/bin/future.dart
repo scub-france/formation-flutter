c0b01bf0e4ae073b99e9a9a2b0c75a0a6d9bf381
ddeshayes <dayann.deshayes@scub.net>
2023-04-04 17:15:03 +0200

    amelioration example

--- console/future/isolate/bin/future.dart
+++ console/future/isolate/bin/future.dart
@@ -1,22 +1,46 @@
 import 'dart:isolate';
 
 void main() async {
-  // Créer un nouveau ReceivePort pour écouter les messages
-  final receivePort = ReceivePort();
+  // créer un port pour communiquer entre les isolates
+  final p = ReceivePort();
 
-  // Démarrer un nouvel Isolate et récupérer son SendPort
-  final isolate = await Isolate.spawn(isolateFunction, receivePort.sendPort);
+  // créer un map pour envoyer les données avec le port
+  final data = {'port': p.sendPort, 'data' : 'here is some data.'};
 
-  // Attendre la réponse de l'Isolate
-  final response = await receivePort.first;
-  print('Received response: $response');
+  // création de l'isolate
+  final isolate = await Isolate.spawn(heavyFunction, data);
 
-  print('je suis dans le main');
-  // Tuer l'Isolate
-  isolate.kill();
+  // tu peux récupérer seulement le premier élément comme ça
+  //final computedData = await p.first;
+  
+  // mais si tu veux récupérer plus d'éléments tu peux utiliser la méthode listen
+  //on va récupérer les données envoyées par le port ainsi que le message qu'on lui à envoyé en premier
+  p.listen((message) {
+    print(message);
+  });
+
+  // ou tu peux utiliser un for
+  // await for (var item in p) {
+  //   print(item);
+  // }
 }
 
-void isolateFunction(SendPort sendPort) {
-  // Envoyer une réponse au main isolate
-  Future.delayed(Duration(seconds: 5),() {sendPort.send('j\'ai executer un calcul de 5s');});
+// Top Level Function
+void heavyFunction(Map<String, dynamic> map) {
+  // récupérer les données du map
+  final SendPort port = map['port'];
+  final String data = map['data'];
+
+  // Heavy computing process
+  final computedData = data;
+  print('je suis dans le heavyFunction, est voici la data : $computedData');
+
+  // tu peux envoyer des données avec la methode send du port
+  port.send('data1');
+  port.send('data2');
+  port.send('data3');
+
+  // N'oubliez pas  de kill l'isolate afin d'évite tout fuite mémoire ou performance
+  // appart si vous utiliser le compute de flutter qui gère tout ça pour vous
+  Isolate.exit(port, computedData);
 }
