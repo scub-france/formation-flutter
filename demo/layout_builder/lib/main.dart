f5b14008ca49f9f7d0c9cff0dd5f8530022b3fa6
loghann.mussard <loghann.mussard@scub.net>
2023-04-11 10:07:26 +0200

    refacto du Main et du README

--- demo/layout_builder/lib/main.dart
+++ demo/layout_builder/lib/main.dart
@@ -19,13 +19,12 @@ class MyStatelessWidget extends StatelessWidget {
   const MyStatelessWidget({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    // Afficher une AppBar avec le titre "LayoutBuilder Example"
-    return Scaffold(
+  build(context) =>
+    Scaffold(
       appBar: AppBar(title: const Text('LayoutBuilder Example')),
       // Utiliser le LayoutBuilder pour rendre le widget adaptable à la taille du parent
       body: LayoutBuilder(
-        // La fonction de rappel prend en entrée un contexte de construction (BuildContext) et un objet BoxConstraints qui représente les limites de taille maximale et minimale que le widget peut avoir
+        // La fonction de rappel (builder) prend en entrée un contexte de construction (BuildContext) et un objet BoxConstraints qui représente les limites de taille maximale et minimale que le widget peut avoir
         builder: (BuildContext context, BoxConstraints constraints) {
           // Si la largeur maximale disponible est supérieure à 600 pixels, afficher une rangée de deux conteneurs (un rouge et un jaune)
           // Essayez de changer le `maxWidth` en `minWidth`, ou, changer le nombre de pixel et voyez le résultat
@@ -58,5 +57,4 @@ class MyStatelessWidget extends StatelessWidget {
         },
       ),
     );
-  }
 }
