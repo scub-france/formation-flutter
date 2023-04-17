ecea6cf4627a63b0c7226b47f6a33072092148a7
Robert Felker <felker.robert@scub.net>
2023-04-17 15:38:32 +0200

    LGTM

--- console/console_extension/lib/console_extension.dart
+++ console/console_extension/lib/console_extension.dart
@@ -1,15 +1,21 @@
-/// Dans cet exemple, je vous montre comment implementer une extension pratique
-/// En effet, le type Iterable na pas de methode removeWhere(....), il faut ainsi caster notre Iterable el List
-/// pour pouvoir avoir accées a la methode removeWhere.... pourquoi ne pas automatiser ca dans une extention!?
-extension Remove<E> on Iterable<E> {
+/// Comment implementer une extension pratique
+/// Le type Iterable na pas de méthode removeWhere(....), il faut ainsi caster notre Iterable en List
+/// pour pouvoir avoir accéder a la methode removeWhere.... pourquoi ne pas automatiser ca dans une extension!?
+extension ExtendedList<E> on Iterable<E> {
   Iterable<E> deleteWhere(bool Function(E element) test) => toList()..removeWhere(test);
 }
 
-
 void main(List<String> arguments) {
-  Iterable<int> iterable= Iterable.generate(10, (index) => index);
-  /// sans extension on aurait prcédés comme suite:
-  /// final result= iterable.toList()..removeWhere((element) => element%2==0);
-  final result= iterable.deleteWhere((element) => element%2==0);
-  print(result);
+  print("Génération d'une liste");
+  Iterable<int> iterable = Iterable.generate(10, (index) => index);
+  // sans extension nous aurions du procédé ainsi:
+  // final result= iterable.toList()..removeWhere((element) => element%2==0);
+
+  print("Liste originale $iterable");
+
+  final result = iterable.deleteWhere((element) => element % 2 == 0);
+  print('Liste nettoyée: $result');
+
+  final found = result.contains(2);
+  print('Contient 2? $found');
 }
