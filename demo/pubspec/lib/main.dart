b85b176ba84c395dff87d69605bb6dd17ed42fb8
loghann.mussard <loghann.mussard@scub.net>
2023-04-27 17:20:38 +0200

    refacto v1 du main et pubspec.yaml

--- demo/pubspec/lib/main.dart
+++ demo/pubspec/lib/main.dart
@@ -1,6 +1,9 @@
 
-
-// Vous trouverez le fichier 'pubscpec.yaml' dans le menu de gauche.
+// Le fichier qui nous interrèsse ici est pubspec.yaml
+// Vous trouverez ce fichier dans le menu de gauche.
 //
 // Si vous ne le voyez pas, cliquez sur le menu burger, dans l'onglet où il y a 'main', afin de le dérouler et de voir le fichier
 
+void main() {
+
+}
