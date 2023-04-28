d717ee3a5f621536da1b670c304c294fb44638d1
Robert Felker <felker.robert@scub.net>
2023-04-28 10:11:44 +0200

    rewrite of equatable

--- console/equatable_lesson/lib/main.dart
+++ console/equatable_lesson/lib/main.dart
@@ -1,29 +1,34 @@
 import 'package:equatable/equatable.dart';
 
-// La classe Person hérite de la classe Equatable
-class Person extends Equatable {
-  // Les champs de la classe Person
-  final String name;
+// Etendre Equatable
+class Utilisateur extends Equatable {
+  final String nom;
   final int age;
-  final String address;
 
-  // Le constructeur de la classe Person
-  const Person({required this.name, required this.age, required this.address});
+  // Constructeur
+  const Utilisateur({required this.nom, required this.age});
 
-  // La méthode get props renvoie une liste des champs qui doivent être pris en compte pour la comparaison
+  // Sélection des propriétés pour comparaison
   @override
-  List<Object> get props => [name, age, address];
+  List<Object> get props => [nom, age];
+
+  // Bénéficier de toString()
+  @override
+  bool get stringify => true;
 }
 
 void main() {
-  // Création de trois instances de la classe Person
-  const person1 = Person(name: 'John', age: 30, address: '123 Main St');
-  const person2 = Person(name: 'John', age: 30, address: '123 Main St');
-  const person3 = Person(name: 'Jean', age: 30, address: '123 Main St');
+  // Création de trois instances de la classe Utilisateur
+  final u1 = Utilisateur(nom: 'John', age: 30);
+  final u2 = Utilisateur(nom: 'John', age: 30);
+  final u3 = Utilisateur(nom: 'Jean', age: 43);
 
   // On peut utiliser l'opérateur == pour comparer les instances
-  print(person1 == person2); // sera égal à true
-  print(person1 == person3); // sera égale à false
+  // Les instances sont différents mais la comparaison est OK
+  print(u1 == u2);
+  // Valeur sont bien différentes
+  print(u1 == u3);
 
-  //Vous pouvez essayer d'instancier d'autre Person et de les comparer
+  // Bénéficier de toString()
+  print(u1);
 }
