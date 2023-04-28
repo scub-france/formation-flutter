0fc5dbaa7e8fe93f7993b1df9313c962c632e1e1
loghann.mussard <loghann.mussard@scub.net>
2023-04-28 11:36:17 +0200

    correction orthographe

--- demo/pubspec/lib/main.dart
+++ demo/pubspec/lib/main.dart
@@ -1,8 +1,8 @@
 
-// Le fichier qui nous interrèsse ici est pubspec.yaml
+// Le fichier qui nous intéresse ici est pubspec.yaml.
 // Vous trouverez ce fichier dans le menu de gauche.
 //
-// Si vous ne le voyez pas, cliquez sur le menu burger, dans l'onglet où il y a 'main', afin de le dérouler et de voir le fichier
+// Si vous ne le voyez pas, cliquez sur le menu burger situé dans l'onglet où il y a 'main', afin de le dérouler et de voir le fichier.
 
 void main() {
 
