06b80ff237e11c4a1cc8d1ff70f63e3ca239f231
Robert Felker <felker.robert@scub.net>
2023-04-28 10:47:40 +0200

    refactor avec rethrow, finally, etc full exemple

--- console/console_error/lib/main.dart
+++ console/console_error/lib/main.dart
@@ -1,26 +1,37 @@
-// Console_error
-void main() {
-  //En Dart, vous pouvez utiliser TRY-CATCH-FINALLY pour intercepter les erreurs qui se produisent lors de l'exécution de votre programme
-  try {
-    var result = divide(10, 0); // Appel de la fonction 'divide' avec les arguments 10 et 0
-    print(result);
-  } catch (e) { // Le bloc catch intercepte l'erreur et affiche un message
-    print("Une erreur s'est produite : $e");
-  } finally { // Le bloc finally s'exécute toujours, que l'erreur se produise ou non
-    print("Le programme a terminé son exécution.");
-  }
-
-  var age = 24;
-  // les ASSERTIONS (assert) sont utilisées pour vérifier les conditions qui doivents être vraies pour que votre code fonctionne correctement
-  // Essayez de changer les valeurs de l'assert ou de la variable 'age' pour ne pas remplir la condition et compilez le code
-  assert(age >= 18); // L'assertion vérifie que l'âge est supérieur ou égal à 18 dans ce cas
-  print("L'âge est valide : $age");
+/// Un type d'Exception explicite
+/// avec un message pour simplifier la gestion des exceptions
+class ContenuInvalideException implements Exception {
+  final String message;
+  const ContenuInvalideException([this.message = ""]);
 }
 
-double divide(double a, double b) {
-  if (b == 0) { // Si le deuxième nombre est zéro, une erreur est générée avec l'instruction throw
-    // Essayez de changer les paramètres de la méthode 'divide' et regarder le résultat
-    throw "Impossible de diviser par zéro.";
+void main() {
+  try {
+    // Communication va échouer mais être récupérée
+    communication();
+  } catch (e) {
+    // Rethrow nous permet un second niveau de gestion
+    print("Tentative de communication interrompue");
   }
-  return a / b; // Sinon, on effectue la division et on retourne le résultat
+}
+
+/// Simuler un message reçu par un utilisateur à un agent
+void communication() {
+  try {
+    final valeurUtilisateur = 'Pouvez-vous traiter ma demande';
+    // Le message n'est pas valide => throws
+    confirmation(valeurUtilisateur);
+  } on ContenuInvalideException catch (e) {
+    // On traite spécifiquement le contenu invalide
+    print("Pensez à saluer nos agents");
+    rethrow;
+  } finally {
+    // Le bloc finally s'exécute toujours, que l'erreur se produise ou non
+    print("Confirmation de prise de contact avec un agent.");
+  }
+}
+
+String confirmation(String message, {String required = 'Bonjour'}) {
+  if (!message.contains(required)) throw ContenuInvalideException('Non trouvé: $required');
+  return message;
 }
