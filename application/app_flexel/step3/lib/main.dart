3a136989daf7645a7c98b24138e9f1b92cc002d9
karim-lamtara <karim.lamtara@scub.net>
2023-04-28 11:39:09 +0200

    step3

--- application/app_flexel/step3/lib/main.dart
+++ application/app_flexel/step3/lib/main.dart
@@ -4,15 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:pluto_grid/pluto_grid.dart';
 
 /// Dans ce step, on verra comment ajouter dynamiquement des lignes,
-/// la gestion des évènements ainsi que l'intégration de Formula.js,
-/// Il s'agit d'une librairie qui nous permets d'utiliser des fonctions excel.
-/// Pour intégrer un module js dans flutter, il faut le faire dans index.html.
-/// Je vous invite a le consulter (/web/index.html).
-/// Il suffit ainsi d'ajouter ces deux lignes de codes entres les balises
-/// <head></head> :
-/// <script src="https://cdn.jsdelivr.net/npm/jstat@1.9.2/dist/jstat.min.js"></script>
-/// <script src="https://cdn.jsdelivr.net/gh/formulajs/formulajs@2.9.3/dist/formula.min.js"></script>
-/// Ensuite, noubliez pas de l'importer dans votre main : import 'dart:js' as js;
+/// formater le contenu des colonnes et la gestion des évènements.
 void main() {
   runApp(MaterialApp(
     title: 'Flutter Demo',
@@ -52,50 +44,52 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
     // définir les colonnes du tableau excel
     // Le constructeur du plutoGrid a besoin d'un paramètre column qui prend
     // une liste de PlutoColumn.
     List<PlutoColumn> columns = [
       PlutoColumn(
-        // Le contenu de title sera affiché dans la vue
         title: 'Sprint',
-
-        // Le contenu de field nous permets d'accéder a notre colonne.
-        // On peut dire que c'est le nom LOGIQUE
         field: 'sprint',
-
-        // On définie ici le type du contenu de nos cellules
         type: PlutoColumnType.text(),
       ),
       PlutoColumn(
           title: 'Nbr_dev',
           field: 'nbrDev',
-
-          // Le contenu de Nbr_dev est logiquement un nombre, on aurait pu
-          // le définir comme tel: PlutoColumnType.number().
-          // On a opté pour type text pour les besoin de l'application,
-          // On peut ainsi mettre des ***** dans la dernière ligne.
-          type: PlutoColumnType.text()),
+          type: PlutoColumnType.text(),
+          formatter: (v) {
+            if (double.tryParse(v) != null)
+              return "$v devs";
+            else
+              return v;
+          }),
       PlutoColumn(
-        title: 'Nbr_jour',
-        field: 'nbrJour',
-
-        // Le contenu de Nbr_dev est logiquement un nombre, on aurait pu
-        // le définir comme tel: PlutoColumnType.number().
-        // On a opté pour le type text pour les besoin de l'application,
-        // On peut ainsi mettre des ***** dans la dernière ligne.
-        type: PlutoColumnType.text(),
-      ),
+          title: 'Nbr_jour',
+          field: 'nbrJour',
+          type: PlutoColumnType.text(),
+          formatter: (v) {
+            if (double.tryParse(v) != null)
+              return "$v j";
+            else
+              return v;
+          }),
       PlutoColumn(
-        title: 'jours/homme',
-        field: 'jourHomme',
-
-        // Le contenu de Nbr_dev est logiquement un nombre, on aurait pu
-        // le définir comme tel: PlutoColumnType.number().
-        // On a opté pour type text pour les besoin de l'application,
-        // On peut ainsi mettre des ***** dans la dernière ligne.
-        type: PlutoColumnType.text(),
-      ),
+          title: 'jours/homme',
+          field: 'jourHomme',
+          // Une autre manière de formatage consiste a modifier le comportement
+          // du type en utilisant les paramètres de son constructeur. Cette
+          // méthode peut s'avérer tres pratique quand on a plusieurs colonnes
+          // qui partagent le meme comportement.
+          // On commence par construire notre propre type:
+          // final monNumberType= PlutoColumnType.number(.........);
+          // Ensuite ,il suffit d'utiliser "monNumberType" dans le constructeur des colonne:
+          // PlutoColumn(type: monNumberType, title:........)
+          type:  PlutoColumnType.number(
+            defaultValue: 0,
+            format: "###,###.### j/h",
+          ),
+      )
     ];
 
     // On ajoute les lignes qui seront affichées au démarrage de l'applications.
@@ -105,16 +99,17 @@ class _MyHomePageState extends State<MyHomePage> {
           'sprint': PlutoCell(value: "Sprint$numberSprint"),
           'nbrDev': PlutoCell(value: "0"),
           'nbrJour': PlutoCell(value: "0"),
-          'jourHomme': PlutoCell(value: "0"),
+          'jourHomme': PlutoCell(),
         },
       );
     }
+
     PlutoRow totalRow = PlutoRow(
       cells: {
         'sprint': PlutoCell(value: "*****"),
         'nbrDev': PlutoCell(value: "*****"),
         'nbrJour': PlutoCell(value: "TOTAL"),
-        'jourHomme': PlutoCell(value: 0),
+        'jourHomme': PlutoCell(),
       },
     );
 
@@ -127,37 +122,44 @@ class _MyHomePageState extends State<MyHomePage> {
     PlutoGrid maGrille = PlutoGrid(
         columns: columns,
         rows: rows,
-        // onLoaded nous permet de faire des actions quand le widget se charge
+        // onLoaded nous permet de faire des actions quand le widget et chargé
         // Il nous permet ici d'initialiser notre stateManager
         onLoaded: (PlutoGridOnLoadedEvent event) {
           stateManager = event.stateManager;
           // On indique ici au stateManager d'empêcher l'utilisateur d'éditer
           // et donc de changer les colones sprint et jours/hommes
-          stateManager.columns[0].enableEditingMode= false;
-          stateManager.columns[3].enableEditingMode= false;
+          stateManager.columns[0].enableEditingMode = false;
+          stateManager.columns[3].enableEditingMode = false;
         },
         // onChanged est appelé quand la valeur de l'une des cellules du tableau
         // change.
         onChanged: (PlutoGridOnChangedEvent event) {
+
           // Grace a cette condition, seul les changement qui affectent les
-          // cellules qui n'appartiennent pas a la dernière ligne seront traités
+          // cellules qui n'appartiennent pas a la dernière ligne (totalRow)
+          // seront traités
           if (event.rowIdx != stateManager.rows.length - 1) {
             final nbrDev =
                 stateManager.rows[event.rowIdx].cells['nbrDev']?.value;
             final nbrJour =
                 stateManager.rows[event.rowIdx].cells['nbrJour']?.value;
+            // formula.js fera les calcul pour nous.
             final totalRow = js.context
                 .callMethod('eval', ['formulajs.PRODUCT([$nbrDev, $nbrJour])']);
             stateManager.rows[event.rowIdx].cells['jourHomme']?.value =
-            "$totalRow";
+                "$totalRow";
+            // totaux contiendra une liste des valeurs de la colonne jour_homme
+            // sauf la dernière cellule.
             List totaux = [];
             for (int i = 0; i < stateManager.rows.length - 1; i++) {
               final jourHommeRow =
                   stateManager.rows[i].cells['jourHomme']?.value;
               totaux.add(jourHommeRow);
             }
+            // On fourni la liste totaux a formula.js pour qu'il applique
+            // la methode SUM().
             stateManager.rows[stateManager.rows.length - 1].cells['jourHomme']
-                ?.value =
+                    ?.value =
                 js.context.callMethod('eval', ['formulajs.SUM([$totaux])']);
           }
 
@@ -165,9 +167,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // peuvent changer on traite ici le cas si l'user change
           // leurs valeur, on remet ainsi les anciennes valeurs en cas de
           // changement.
-          else{
-            stateManager.rows[event.rowIdx].cells['nbrDev']?.value='*****';
-            stateManager.rows[event.rowIdx].cells['nbrJour']?.value='TOTAL';
+          else {
+            stateManager.rows[event.rowIdx].cells['nbrDev']?.value = '*****';
+            stateManager.rows[event.rowIdx].cells['nbrJour']?.value = 'TOTAL';
           }
         });
 
@@ -175,9 +177,9 @@ class _MyHomePageState extends State<MyHomePage> {
         appBar: AppBar(
           title: const Center(
               child: Text(
-                'Flexel',
-                style: TextStyle(fontSize: 40),
-              )),
+            'Flexel',
+            style: TextStyle(fontSize: 40),
+          )),
         ),
         body: Container(
           padding: const EdgeInsets.all(15),
@@ -186,13 +188,12 @@ class _MyHomePageState extends State<MyHomePage> {
         floatingActionButton: FloatingActionButton(
           onPressed: () {
             // Permet d'ajouter une ligne a notre tableau
-            stateManager.insertRows(compRow, [voidRow(compRow+1)]);
+            stateManager.insertRows(compRow, [voidRow(compRow + 1)]);
             setState(() {
-              compRow= compRow+1;
+              compRow = compRow + 1;
             });
           },
           child: const Icon(Icons.add),
-        )
-    );
+        ));
   }
 }
