c35a13d6efab30b277350c68ae8928994248b5d4
Robert Felker <felker.robert@scub.net>
2023-05-02 10:29:24 +0200

    refactor flexel step1

--- application/app_flexel/step1/lib/main.dart
+++ application/app_flexel/step1/lib/main.dart
@@ -1,132 +1,92 @@
-
 import 'package:flutter/material.dart';
 import 'package:pluto_grid/pluto_grid.dart';
 
-/// La librairie pluto_grid permet de dessiner et manipuler
-/// un tableau excel.
-/// C'est une librairie! pensez a la déclarer dans pubspec.yaml et
-/// a son import (voir plus haut).
-/// Dans ce step, on commence par la création d'un widget simple,
-/// on verra la gestion des saisies et des évènements dans le step2.
-void main() => runApp( MaterialApp(
-    theme: ThemeData(
-      primarySwatch: Colors.blue,
+/*
+
+  PlutoGrid est un Widget complexe qui permet de créer des tableaux
+  configurable sur tous les aspects.
+
+ */
+
+class PlutoGridDemo extends StatelessWidget {
+  PlutoGridDemo({Key? key}) : super(key: key);
+
+  // 1. Définition de nos colonnes
+  static final columns = [
+    PlutoColumn(
+      width: 80,
+      // Titre de la Colonne
+      title: 'Saison',
+      // Nom du champs
+      field: 'season',
+      // Type de cellule
+      type: PlutoColumnType.text(),
     ),
-    home: const PlutoGridPage(),));
-
-class PlutoGridPage extends StatefulWidget {
-  const PlutoGridPage({super.key});
-
-  @override
-  State<PlutoGridPage> createState() => _PlutoGridPageState();
-}
-
-class _PlutoGridPageState extends State<PlutoGridPage> {
-  late int compRow;
-  final int totalJourHomme=0;
-
-  @override
-  void initState() {
-    super.initState();
-    compRow = 1;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    // définir les colonnes du tableau exel
-    // Le constructeur du plutoGrid a besoin d'un paramètre column qui prend
-    // une liste de PlutoColumn.
-    List<PlutoColumn> columns = [
-      PlutoColumn(
-        width: 80,
-        // Le contenu de title sera affiché dans la vue
-        title: 'Sprint',
-
-        // Le contenu de field nous permets d'accéder a notre colonne.
-        // On peut dire que c'est le nom LOGIQUE
-        field: 'sprint',
-
-        // On définie ici le type du contenu de nos cellules
-        type: PlutoColumnType.text(),
-      ),
-
-      PlutoColumn(
-          width: 90,
-          title: 'Nbr_dev',
-          field: 'nbrDev',
-
-          // Le contenu de Nbr_dev est logiquement un nombre, on aurait pu
-          // le définir comme tel: PlutoColumnType.number().
-          // On a opté pour type text pour les besoin de l'application,
-          // On peut ainsi mettre des ***** dans la dernière ligne.
-          type: PlutoColumnType.text()),
-
-      PlutoColumn(
+    PlutoColumn(
+      width: 90,
+      title: 'Equipe',
+      field: 'team',
+      type: PlutoColumnType.text(),
+    ),
+    PlutoColumn(
         width: 90,
-        title: 'Nbr_jour',
-        field: 'nbrJour',
+        title: 'Score',
+        field: 'score',
+        type: PlutoColumnType.number(),
+        footerRenderer: (rendererContext) => PlutoAggregateColumnFooter(
+            rendererContext: rendererContext,
+            type: PlutoAggregateColumnType.sum,
+            format: '#,###',
+            alignment: Alignment.center,
+            titleSpanBuilder: (text) => [
+                  const TextSpan(text: 'Total', style: TextStyle(color: Colors.red)),
+                  const TextSpan(text: ' : '),
+                  TextSpan(text: text),
+                ]))
+  ];
 
-        // Le contenu de Nbr_dev est logiquement un nombre, on aurait pu
-        // le définir comme tel: PlutoColumnType.number().
-        // On a opté pour le type text pour les besoin de l'application,
-        // On peut ainsi mettre des ***** dans la dernière ligne.
-        type: PlutoColumnType.text(),
-      ),
-
-      PlutoColumn(
-        width: 130,
-        title: 'Jours/Homme',
-        field: 'jourHomme',
-
-        // Le contenu de Nbr_dev est logiquement un nombre, on aurait pu
-        // le définir comme tel: PlutoColumnType.number().
-        // On a opté pour type text pour les besoin de l'application,
-        // On peut ainsi mettre des ***** dans la dernière ligne.
-        type: PlutoColumnType.text(),
-      ),
-    ];
-
-    // On ajoute les lignes qui seront affichées au démarrage de l'applications.
-    PlutoRow voidRow(int numberSprint) => PlutoRow(
-          cells: {
-            'sprint': PlutoCell(value: "Sprint$numberSprint"),
-            'nbrDev': PlutoCell(value: "0"),
-            'nbrJour': PlutoCell(value: "0"),
-            'jourHomme': PlutoCell(value: "0"),
-          },
-        );
-    PlutoRow totalRow = PlutoRow(
+  final rows = [
+    PlutoRow(
       cells: {
-        'sprint': PlutoCell(value: ""),
-        'nbrDev': PlutoCell(value: ""),
-        'nbrJour': PlutoCell(value: "TOTAL"),
-        'jourHomme': PlutoCell(value: 0),
+        'season': PlutoCell(value: "22"),
+        'team': PlutoCell(value: "White"),
+        'score': PlutoCell(value: 8),
       },
-    );
+    ),
+    PlutoRow(
+      cells: {
+        'season': PlutoCell(value: "22"),
+        'team': PlutoCell(value: "Blue"),
+        'score': PlutoCell(value: 9),
+      },
+    ),
+    PlutoRow(
+      cells: {
+        'season': PlutoCell(value: "23"),
+        'team': PlutoCell(value: "White"),
+        'score': PlutoCell(value: 10),
+      },
+    ),
+    PlutoRow(
+      cells: {
+        'season': PlutoCell(value: "23"),
+        'team': PlutoCell(value: "Blue"),
+        'score': PlutoCell(value: 9),
+      },
+    ),
+  ];
 
-    // Le constructeur du plutoGrid a besoin d'un paramètre Row qui prend
-    // une liste de PlutoRow.
-    List<PlutoRow> rows = [voidRow(compRow), totalRow];
-
-
-    PlutoGrid maGrille = PlutoGrid(
-        columns: columns,
-        rows: rows,
-    );
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-            child: Text(
-          'Flexel',
-          style: TextStyle(fontSize: 40),
-        )),
-      ),
-      body: Container(
+  @override
+  build(context) => Container(
         padding: const EdgeInsets.all(15),
-        child: maGrille,
-      ),
-    );
-  }
+        child: PlutoGrid(
+            columns: columns,
+            rows: rows,
+            rowColorCallback: (rowColorContext) => rowColorContext.row.cells.entries.elementAt(2).value.value > 9
+                ? Colors.blueAccent
+                : Colors.transparent),
+      );
 }
+
+void main() => runApp(
+    MaterialApp(home: Scaffold(appBar: AppBar(title: const Center(child: Text('PlutoGrid'))), body: PlutoGridDemo())));
