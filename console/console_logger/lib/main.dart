92b49fa5844ff40ca218d2934d585a30b3fd99f8
ddeshayes <dayann.deshayes@scub.net>
2023-05-09 13:26:05 +0200

    refacto exemple

--- console/console_logger/lib/main.dart
+++ console/console_logger/lib/main.dart
@@ -1,43 +1,94 @@
 import 'package:logging/logging.dart';
 
 final log = Logger('ExampleLogger');
-final Logger myLogger = Logger('MyLogger');
 
 /// Dans ce programme, nous allons configurer le journal pour enregistrer les messages à tous les niveaux de gravité.
-/// Qui sont: SEVERE, WARNING, INFO, CONFIG, FINE, FINER, FINEST
 void main() {
-  // Configuration du logger pour enregistrer les messages à tous les niveaux de gravité
-  Logger.root.level = Level.ALL;
-  // Ecouteur de journalisation, qui enregistre les messages de journalisation dans la console
-  // avec des informations supplémentaires telles que le niveau de gravité, l'heure, le nom du journal, etc.
-  // ce qui n'est pas disponible dans les print() fonctions.
+  // Configuration du root logger pour affichage des messages du niveaux souhaité et supérieur à ce niveau
+  // rappel les niveaux de gravité sont classés du plus élevé au plus bas(le plus bas étant à droite)
+  // OFF, SHOUT, SEVERE, WARNING, INFO, CONFIG, FINE, FINER, FINEST
+  Logger.root.level = Level.WARNING;
+  // Configuration du root logger pour affichage les informations utile du log
   Logger.root.onRecord.listen((record) {
-    //affichage des tout les informations de log
+    //affichage des tout les informations utile de log
+    // il y'en a d'autre comme record.error, record.stackTrace, record.object, record.sequenceNumber, record.time
     print('${record.level.name}: ${record.time}: '
-        '${record.message} , ${record.zone.runtimeType.toString()}, ${record.loggerName}, '
-        '${record.error}, ${record.stackTrace}, ${record.sequenceNumber}, ${record.object}'
-        ' ${record.time.day}');
+        '${record.message} , ${record.zone.runtimeType.toString()}, ${record.loggerName}');
   });
 
-  // Utilisation du logger pour enregistrer des messages de journalisation à différents niveaux de gravité
-  myLogger.severe('Ceci est un message SEVERE');
-  myLogger.warning('Ceci est un message WARNING');
-  myLogger.info('Ceci est un message INFO');
-  myLogger.config('Ceci est un message CONFIG');
-  myLogger.fine('Ceci est un message FINE');
-  myLogger.finer('Ceci est un message FINER');
-  myLogger.finest('Ceci est un message FINEST');
+  // Création des instances des classe, et appel des méthodes de log qui vont afficher chacun des log avec différents niveau de gravité
+  Person person = Person('John', 30);
+  Student student = Student('John', 30, 'MIT');
+  Teacher teacher = Teacher('John', 30, 'Math');
+  Employee employee = Employee('John', 30, 'Google');
 
-  // fonction pour afficher des messages avec un logger différent
-  logMessage();
+  // logPerson avec le niveau INFO
+  Person.logPerson(person);
+  // logStudent avec le niveau SEVERE
+  Student.logStudent(student);
 
+  // logTeacher avec le niveau FINE
+  Teacher.logTeacher(teacher);
+
+  // logEmployee avec le niveau FINEST
+  Employee.logEmployee(employee);
+
+  // try catch pour tester le niveau SHOUT et WARNING
+  try {
+    throw Exception('Exception');
+  } catch (e) {
+    log.shout('Exception: $e');
+    log.warning('Exception: $e');
+  }
 }
 
-void logMessage() {
-  log.warning('message d\'un log différent');
-  log.info('message d\'un log différent');
-  log.info('message d\'un log différent');
+class Person {
+  String name;
+  int age;
+  static final tab = [1, 2, 3];
+  static Logger log = Logger('Person');
+
+  Person(this.name, this.age);
+
+  static logPerson(Person person) {
+    log.info('Person: ${person.name}, ${person.age}');
+  }
 }
+
+class Student extends Person {
+  String school;
+  static Logger log = Logger('Student');
+
+  Student(String name, int age, this.school) : super(name, age);
+
+  static logStudent(Student student) {
+    log.severe('Student: ${student.name}, ${student.age}, ${student.school}');
+  }
+}
+
+class Teacher extends Person {
+  String subject;
+  static Logger log = Logger('Teacher');
+
+  Teacher(String name, int age, this.subject) : super(name, age);
+
+  static logTeacher(Teacher teacher) {
+    log.fine('Teacher: ${teacher.name}, ${teacher.age}, ${teacher.subject}');
+  }
+}
+
+class Employee extends Person {
+  String company;
+  static Logger log = Logger('Employee');
+
+  Employee(String name, int age, this.company) : super(name, age);
+
+  static logEmployee(Employee employee) {
+    log.finest('Employee: ${employee.name}, ${employee.age}, ${employee.company}');
+  }
+}
+
+
 
 
 
