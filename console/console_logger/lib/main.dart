413855282e29d93733aa452e1a40a3bf917604c8
ddeshayes <dayann.deshayes@scub.net>
2023-05-09 15:42:50 +0200

    change exercise

--- console/console_logger/lib/main.dart
+++ console/console_logger/lib/main.dart
@@ -1,101 +1,104 @@
 import 'package:logging/logging.dart';
 
-final log = Logger('ExampleLogger');
-
-/// Dans ce programme, nous allons configurer le journal pour enregistrer
-/// les messages à tous les niveaux de gravité.
 void main() {
   // Configuration du root logger pour affichage des messages
   // du niveaux souhaité et supérieur à ce niveau rappel les
   // niveaux de gravité sont classés du plus élevé
   // au plus bas(le plus bas étant à droite)
   // OFF, SHOUT, SEVERE, WARNING, INFO, CONFIG, FINE, FINER, FINEST
-  Logger.root.level = Level.CONFIG;
-  // Configuration du root logger pour affichage les informations utile du log
+  Logger.root.level = Level.FINER;
+
+  // Configuration du root logger pour afficher les informations utiles du log.
   Logger.root.onRecord.listen((record) {
-    //affichage des tout les informations utile de log
-    // il y'en a d'autre comme record.error, record.stackTrace,
-    // record.object, record.sequenceNumber, record.time
     print('${record.level.name}: ${record.time}: '
         '${record.message} , ${record.zone.runtimeType.toString()}, ${record.loggerName}');
   });
 
-  // Création des instances des classe, et appel des méthodes de log qui vont
-  // afficher chacun des log avec différents niveau de gravité
-  final person = Person('John', 30);
-  final student = Student('John', 30, 'MIT');
-  final teacher = Teacher('John', 30, 'Math');
-  final employee = Employee('John', 30, 'Google');
+  // Création d'une instance de la classe RegisterService et appel de la méthode register().
+  RegisterService(name: 'Api').register();
 
-  // logPerson avec le niveau INFO
-  person.logPerson();
+  // Affichage du nom de chaque logger enfant de root logger.
+  Logger.root.children.forEach((key, log) => print("::Logs:: ${log.name}"));
 
-  // logStudent avec le niveau SEVERE
-  student.logStudent();
-
-  // logTeacher avec le niveau FINE
-  teacher.logTeacher(teacher);
-
-  // logEmployee avec le niveau FINEST
-  employee.logEmployee(employee);
-
-  // try catch pour tester le niveau SHOUT et WARNING
-  try {
-    throw Exception('Exception');
-  } catch (e) {
-    log.shout('Exception: $e');
-    log.warning('Exception: $e');
-  }
+  // Enregistrement d'un message de niveau "shout,qui valide la fin des enregistrement.
+  Logger.root.shout("Enregistrement complet");
 }
 
-class Person {
+/// Classe abstraite définissant une propriété "name" et un logger "log".
+abstract class Service {
   final String name;
-  final int age;
-  final log = Logger('Person');
+  late Logger log;
 
-  Person(this.name, this.age){
-    log.config("Création de $runtimeType");
+  // constructeur de la classe Service qui prend en paramètre le nom du service.
+  Service({required this.name}) {
+    log = Logger(name);
+    log.info("Création de $runtimeType");
   }
 
-  void logPerson() {
-    log.info('Person: $name, $age');
+  // Méthode abstraite qui sera implémentée par les classes filles.
+  void register();
+}
+
+/// Classe qui étend Service et enregistre plusieurs BackupService.
+class RegisterService extends Service {
+  // Création des instances des classes BackupService et DisabledService.
+  final zone1 = BackupService(name: 'Zone1');
+  final zone2 = BackupService(name: 'Zone2');
+  final zone3 = DisabledService(name: 'Zone3');
+
+  RegisterService({required super.name}) {
+    log.finest("Instance $runtimeType démarrée");
+  }
+
+  // Méthode qui enregistre les instances des classes BackupService et DisabledService.
+  // et qui affiche un message pour chaque zone.
+  @override
+  void register() {
+    log.info('$runtimeType register()');
+    for (final zone in [zone1, zone2, zone3]) {
+      log.fine("Enregistrement par zone $zone");
+      zone.register();
+    }
   }
 }
 
-class Student extends Person {
-  final String school;
-  final log = Logger('Student');
+/// Classe qui étend Service et qui enregistre simplement son état.
+class BackupService extends Service {
+  BackupService({required super.name});
 
-  Student(String name, int age, this.school) : super(name, age);
+  // Méthode qui enregistre le nom de la classe.
+  @override
+  void register() => log.info('$runtimeType register()');
 
-  void logStudent() {
-    log.severe('Student: $name , $age, $school');
+  // Override de la méthode toString() pour afficher le nom de la classe.
+  @override
+  toString() => '[Service $name]';
+}
+
+/// Classe qui étend BackupService et qui enregistre un message en cas de dégradation du service réseau.
+class SlowService extends BackupService {
+  SlowService({required super.name});
+
+  // Override de la méthode register() et enregistre un message de niveau "warning".
+  @override
+  void register() {
+    log.warning("Le service réseau est dégradé");
+    super.register();
   }
 }
 
-class Teacher extends Person {
-  final String subject;
-  final log = Logger('Teacher');
+/// Classe qui étend BackupService et qui lance une exception pour simuler un service en panne.
+class DisabledService extends BackupService {
+  DisabledService({required super.name});
 
-  Teacher(String name, int age, this.subject) : super(name, age);
-
-  void logTeacher(Teacher teacher) {
-    log.fine('Teacher: $name, $age, $subject');
+  // Override de la méthode register() et lance une exception, qui sera gérée
+  // par le bloc catch en affichant un message de niveau "severe".
+  @override
+  void register() {
+    try {
+      throw Exception("Service en panne;");
+    } catch (e) {
+      log.severe(e.toString());
+    }
   }
 }
-
-class Employee extends Person {
-  final String company;
-  final log = Logger('Employee');
-
-  Employee(String name, int age, this.company) : super(name, age);
-
-  void logEmployee(Employee employee) {
-    log.finest('Employee: $name, $age, $company');
-  }
-}
-
-
-
-
-
