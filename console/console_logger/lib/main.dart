afcdc1390d44359a6fd323ec867a16cb10ad0347
ddeshayes <dayann.deshayes@scub.net>
2023-05-09 13:54:50 +0200

    REFACTO exercise

--- console/console_logger/lib/main.dart
+++ console/console_logger/lib/main.dart
@@ -2,36 +2,42 @@ import 'package:logging/logging.dart';
 
 final log = Logger('ExampleLogger');
 
-/// Dans ce programme, nous allons configurer le journal pour enregistrer les messages à tous les niveaux de gravité.
+/// Dans ce programme, nous allons configurer le journal pour enregistrer
+/// les messages à tous les niveaux de gravité.
 void main() {
-  // Configuration du root logger pour affichage des messages du niveaux souhaité et supérieur à ce niveau
-  // rappel les niveaux de gravité sont classés du plus élevé au plus bas(le plus bas étant à droite)
+  // Configuration du root logger pour affichage des messages
+  // du niveaux souhaité et supérieur à ce niveau rappel les
+  // niveaux de gravité sont classés du plus élevé
+  // au plus bas(le plus bas étant à droite)
   // OFF, SHOUT, SEVERE, WARNING, INFO, CONFIG, FINE, FINER, FINEST
-  Logger.root.level = Level.WARNING;
+  Logger.root.level = Level.CONFIG;
   // Configuration du root logger pour affichage les informations utile du log
   Logger.root.onRecord.listen((record) {
     //affichage des tout les informations utile de log
-    // il y'en a d'autre comme record.error, record.stackTrace, record.object, record.sequenceNumber, record.time
+    // il y'en a d'autre comme record.error, record.stackTrace,
+    // record.object, record.sequenceNumber, record.time
     print('${record.level.name}: ${record.time}: '
         '${record.message} , ${record.zone.runtimeType.toString()}, ${record.loggerName}');
   });
 
-  // Création des instances des classe, et appel des méthodes de log qui vont afficher chacun des log avec différents niveau de gravité
-  Person person = Person('John', 30);
-  Student student = Student('John', 30, 'MIT');
-  Teacher teacher = Teacher('John', 30, 'Math');
-  Employee employee = Employee('John', 30, 'Google');
+  // Création des instances des classe, et appel des méthodes de log qui vont
+  // afficher chacun des log avec différents niveau de gravité
+  final person = Person('John', 30);
+  final student = Student('John', 30, 'MIT');
+  final teacher = Teacher('John', 30, 'Math');
+  final employee = Employee('John', 30, 'Google');
 
   // logPerson avec le niveau INFO
-  Person.logPerson(person);
+  person.logPerson();
+
   // logStudent avec le niveau SEVERE
-  Student.logStudent(student);
+  student.logStudent();
 
   // logTeacher avec le niveau FINE
-  Teacher.logTeacher(teacher);
+  teacher.logTeacher(teacher);
 
   // logEmployee avec le niveau FINEST
-  Employee.logEmployee(employee);
+  employee.logEmployee(employee);
 
   // try catch pour tester le niveau SHOUT et WARNING
   try {
@@ -43,47 +49,49 @@ void main() {
 }
 
 class Person {
-  String name;
-  int age;
-  static Logger log = Logger('Person');
+  final String name;
+  final int age;
+  final Logger log = Logger('Person');
 
-  Person(this.name, this.age);
+  Person(this.name, this.age){
+    log.config("Création de $runtimeType");
+  }
 
-  static logPerson(Person person) {
-    log.info('Person: ${person.name}, ${person.age}');
+  void logPerson() {
+    log.info('Person: $name, $age');
   }
 }
 
 class Student extends Person {
-  String school;
-  static Logger log = Logger('Student');
+  final String school;
+  final Logger log = Logger('Student');
 
   Student(String name, int age, this.school) : super(name, age);
 
-  static logStudent(Student student) {
-    log.severe('Student: ${student.name}, ${student.age}, ${student.school}');
+  void logStudent() {
+    log.severe('Student: $name , $age, $school');
   }
 }
 
 class Teacher extends Person {
-  String subject;
-  static Logger log = Logger('Teacher');
+  final String subject;
+  final Logger log = Logger('Teacher');
 
   Teacher(String name, int age, this.subject) : super(name, age);
 
-  static logTeacher(Teacher teacher) {
-    log.fine('Teacher: ${teacher.name}, ${teacher.age}, ${teacher.subject}');
+  void logTeacher(Teacher teacher) {
+    log.fine('Teacher: $name, $age, $subject');
   }
 }
 
 class Employee extends Person {
-  String company;
-  static Logger log = Logger('Employee');
+  final String company;
+  final Logger log = Logger('Employee');
 
   Employee(String name, int age, this.company) : super(name, age);
 
-  static logEmployee(Employee employee) {
-    log.finest('Employee: ${employee.name}, ${employee.age}, ${employee.company}');
+  void logEmployee(Employee employee) {
+    log.finest('Employee: $name, $age, $company');
   }
 }
 
